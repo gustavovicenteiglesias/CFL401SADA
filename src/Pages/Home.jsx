a7433d3dc2fce27e55fc2738acce245e0844476c
Gustavo <gviglesias@alumnos.unsada.edu.ar>
2022-06-14 20:39:57 -0300

    textopropueta

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,6 +7,10 @@ const Home =()=>{
                 <h2>Nuestra vision</h2>
                 <p>El objetivo principal de nuestro Centro de Formación es capacitar lo mejor posible a los alumnos para desempeñarse en el campo profesional. Facilitar su adaptación a las modificaciones laborales que puedan producirse a lo largo de su vida. Construir perfiles profesionales y competencias laborales específicas altamente especializadas de cada curso de Formación Laboral.</p>
             </div>
+
+            <div>
+                <h2>PROPUESTA FORMATIVA 2022</h2>
+            </div>
         
         </Container>
     )
